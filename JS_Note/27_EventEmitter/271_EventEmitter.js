fd92f059952d6460904294ebf3e38e7444658079
Brijesh <brijesh@Brijeshs-MacBook-Air.local>
2025-01-26 00:13:13 +0530

    2025-01-26

--- JS_Note/27_EventEmitter/271_EventEmitter.js
+++ JS_Note/27_EventEmitter/271_EventEmitter.js
@@ -1,7 +1,7 @@
 /**
  * https://www.freecodecamp.org/news/how-to-code-your-own-event-emitter-in-node-js-a-step-by-step-guide-e13b7e7908e1/
  * 
- * when emit calls a listener is added to its internal array of listeners.
+ * when emit calls, a listener is added to its internal array of listeners.
  * 
  * Always use .on('error') for error handling
  * 
@@ -29,18 +29,19 @@ event.emit('bar');
 event.emit('bar');
 event.emit('bar');
 
+
+// Intersting newListener event, which is emitted whenever a new listener is added to the event emitter.
 event.once('newListener', (event, listener) => {
   if (event === 'event') {
     // Insert a new listener in front
+    // myEmitter.emit('event');       // Prints: A
     myEmitter.on('event', () => {
       console.log('B');
     });
+    myEmitter.emit('event');          // Prints: B A
   }
 });
 event.on('event', () => {
   console.log('A');
 });
 event.emit('event');
-Prints:
-  B
-  A
