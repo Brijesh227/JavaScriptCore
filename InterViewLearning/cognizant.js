b3249c05e843be6b0029fbded6657be6c7994674
Brijesh <brijesh@Brijeshs-MacBook-Air.local>
2025-01-30 12:25:23 +0530

    2025-01-30

--- InterViewLearning/cognizant.js
+++ InterViewLearning/cognizant.js
@@ -5,23 +5,53 @@
 // find bug in this
 
 function flattenArray(arr) {
-    let result = [];
-    for (let i = 0; i < arr.length; i++) {
-      if (Array.isArray(arr[i])) {
-        result.concat(flattenArray(arr[i]));
-      } else {
-        result.push(arr[i]);
-      }
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      result.concat(flattenArray(arr[i]));
+    } else {
+      result.push(arr[i]);
     }
-    return result;
   }
-   
-  // Example usage:
-  const nestedArray = [1, [2, [3, 4], 5], 6];
-  const result = flattenArray(nestedArray);
-  console.log(result);
+  return result;
+}
+ 
+// Example usage:
+const nestedArray = [1, [2, [3, 4], 5], 6];
+const result = flattenArray(nestedArray);
+console.log(result);
+
+// ans: use result = result.concat(flattenArray(arr[i]));
 
 // write typescript class for this:
+
+class LRUCache {
+  private map: Map<number, number> = new Map();
+  private size: number = 0;
+  private addAtIndex: number = 0;
+  constructor(size: number){
+      this.size = size;
+  }
+
+  put(key: number, val: number) : void {
+      if(this.addAtIndex < this.size) {
+          this.map.set(key,val);
+          ++this.addAtIndex;
+      } else {
+          
+      }
+  }
+
+  get(key: number) : number {
+      let value: number = -1; 
+      if(this.map.has(key)) {
+          value = this.map.get(key) ?? -1;
+      }
+      return value;
+  }
+}
+
+
 
 const lruCache = new LRUCache(2);
 lruCache.put(1, 1); // cache is {1=1}
